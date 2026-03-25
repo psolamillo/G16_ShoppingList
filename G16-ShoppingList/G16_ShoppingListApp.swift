ddbe023bb58735c3030db7ec7fd482d8e1d5926d
Patrick Solamillo <patricksolamillo@proton.me>
2026-03-24 23:20:53 -0400

    initialize project and add splash screen
    - create initial xcode proj structure
    - Creation of SplashView splashscreen
    - add gitignore with xcode artifacts

---
+++ G16-ShoppingList/G16_ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  G16_ShoppingListApp.swift
+//  G16-ShoppingList
+//
+//  Created by user293649 on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct G16_ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
