3d56def143b09e1a62f44ec069630f84aa6b5c0d
Patrick Millares <millareslepatrick@gmail.com>
2026-03-25 16:28:13 -0400

    initialize SwiftData modelContainer in App entry point

--- G16-ShoppingList/G16_ShoppingListApp.swift
+++ G16-ShoppingList/G16_ShoppingListApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct G16_ShoppingListApp: App {
@@ -13,5 +14,6 @@ struct G16_ShoppingListApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: [Category.self, ShoppingItem.self])
     }
 }
